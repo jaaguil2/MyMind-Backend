40b21fa36f2fd18d275595b72f1afbc362a41c15
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-14 13:42:15 -0400

    add hashed passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const User = require('../models/User')
-const { handleValidateId, handleRecordExists } = require('../middleware/custom_errors')
+const bcrypt = require('bcrypt')
 
 const router = express.Router()
 
 // SIGN UP
 // POST /api/signup
 router.post('/signup', (req, res, next) => { 
-  User.create(req.body)
-    .then(user => res.status(201).json(user))
-    .catch(next)
-});
+  bcrypt
+    .hash(req.body.password, 11)
+    .then(hash => ({
+      name: req.body.name,
+      password: hash,
+      userName: req.body.userName,
+      email: req.body.email
+    }))
+  .then(user => User.create(user))
+  .then(user => res.status(201).json(user))
+  .catch(next)
+})
 
 // SIGN IN
 // POST /api/signin
