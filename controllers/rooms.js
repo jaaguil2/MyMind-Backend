4d4eaf3b1f7f6a078f6992802be9d0b7dada1c44
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-14 10:02:29 -0400

    added status handling

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -5,43 +5,66 @@ const router = express.Router()
 
 // INDEX
 // GET api/room
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Room.find()
   .populate('owner')
-    .then((rooms) => res.json(rooms))
-});
+  .then(rooms => res.json(rooms))
+  .catch(next)
+})
 
 // SHOW
 // GET api/room/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   Room.findById(req.params.id)
     .populate('owner')
-    .then((room) => res.json(room))
-});
+    .then(room => {
+      if (!room) {
+        res.sendStatus(404)
+      } else {
+        res.json(room)
+      }
+    })
+    .catch(next)
+})
 
 // CREATE
 // POST api/room
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   Room.create(req.body)
-    .then((room) => res.json(room))
-});
+    .then(room => res.status(201).json(room))
+    .catch(next)
+})
 
 // UPDATE
 // PUT api/room/5a7db6c74d55bc51bdf39793
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   Room.findOneAndUpdate(
     { _id: req.params.id },
     req.body,
     { new: true }
   )
-    .then((room) => res.json(room))
-});
+    .then(room => {
+      if (!room) {
+        res.sendStatus(404)
+      } else {
+        res.json(room)
+      }
+    })
+    .catch(next)
+})
 
 // DESTROY
 // DELETE api/room/5a7db6c74d55bc51bdf39793
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   Room.findOneAndDelete({ _id: req.params.id })
-    .then((job) => res.json(job))
+    .then(room => {
+      if (!room) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(next)
 })
 
 module.exports = router
--- controllers/users.js
+++ controllers/users.js
@@ -5,41 +5,64 @@ const router = express.Router()
 
 // INDEX
 // GET api/user
-router.get('/', (req, res) => { 
+router.get('/', (req, res, next) => {
   User.find()
-    .then((users) => res.json(users))
-});
+    .then(users => res.json(users))
+    .catch(next)
+})
 
 // SHOW
 // GET api/user/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => { 
+router.get('/:id', (req, res, next) => {
   User.findById(req.params.id)
-    .then((user) => res.json(user))
-});
+    .then(user => {
+      if (!user) {
+        res.sendStatus(404)
+      } else {
+        res.json(user)
+      }
+    })
+    .catch(next)
+})
 
 // CREATE
 // POST api/user
-router.post('/', (req, res) => { 
+router.post('/', (req, res, next) => {
   User.create(req.body)
-    .then((user) => res.json(user))
-});
+    .then(user => res.status(201).json(user))
+    .catch(next)
+})
 
 // UPDATE
 // PUT api/user/5a7db6c74d55bc51bdf39793
-router.put('/:id', (req, res) => { 
+router.put('/:id', (req, res, next) => {
   User.findOneAndUpdate(
     { _id: req.params.id }, 
     req.body, 
     { new: true }
   )
-    .then((user) => res.json(user))
-});
+    .then(user => {
+      if (!user) {
+        res.sendStatus(404)
+      } else {
+        res.json(user)
+      }
+    })
+    .catch(next)
+})
 
 // DESTROY
 // DELETE api/user/5a7db6c74d55bc51bdf39793
-router.delete('/:id', (req, res) => { 
+router.delete('/:id', (req, res, next) => {
   User.findOneAndDelete({ _id: req.params.id})
-  .then((job) => res.json(job))
+    .then(user => {
+      if (!user) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(next)
 })
 
 module.exports = router
