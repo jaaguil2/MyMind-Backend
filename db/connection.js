24d6bedeb81227e95625480c551b485615d1f60d
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-21 16:00:09 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'MyMind';
+const MONGODBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'MyMind';
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODBURI)
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
-  .catch((error) => console.log('Connection failed!', error));
+  .catch((error) => console.log('Connection failed!!!', error));
 
 module.exports = mongoose
